1a907c26b7d4f0b5c5ab0d84800bdd0cfb510903
Mohammed Salah <mohammed_salah@icloud.com>
2021-10-13 10:17:02 +0100

    Add student count(1)

--- directory.rb
+++ directory.rb
@@ -11,4 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have \n11 \ngreat students"
+# finally, we print the total number of students
+print "Overall, we have "
+#it's important that print() doesn't add new line characters
+print 11
+puts " great students"
